5544ae42993def9668434427eed6407211a7cd46
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-12-14 12:35:02 +0200

    Update Source.cpp

--- Template_Class/Source.cpp
+++ Template_Class/Source.cpp
@@ -5,17 +5,7 @@ using namespace std;
 
 int main()
 {
-    Array array(10);
-    for (int i = 0; i < 10; i++)
-        array[i] = i + 1;
-    array.resize(7);
-    array.insertBefore(15, 4);
-    array.remove(5);
-    // Äîáàâëÿåì ÷èñëà 35 è 50 â êîíåö è â íà÷àëî
-    array.insertAtEnd(35);
-    array.insertAtBeginning(50);
-    for (int j = 0; j < array.getLength(); j++)
-        std::cout << array[j] << " ";
+    
 
     return 0;
 }
