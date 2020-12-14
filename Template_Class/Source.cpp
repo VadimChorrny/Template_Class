5edf151d8917bf1bff815e97207a4baa974a2f8e
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-12-14 12:34:27 +0200

    Update Source.cpp

--- Template_Class/Source.cpp
+++ Template_Class/Source.cpp
@@ -5,29 +5,17 @@ using namespace std;
 
 int main()
 {
-    // Объявляем массив с 10 элементами
     Array array(10);
-
-    // Заполняем массив числами от 1 до 10
     for (int i = 0; i < 10; i++)
         array[i] = i + 1;
-
-    // Изменяем размер массива до 7 элементов
     array.resize(7);
-
-    // Вставляем число 15 перед элементом с индексом 4
     array.insertBefore(15, 4);
-
-    // Удаляем элемент с индексом 5
     array.remove(5);
-
-    // Добавляем числа 35 и 50 в конец и в начало
+    // Г„Г®ГЎГ ГўГ«ГїГҐГ¬ Г·ГЁГ±Г«Г  35 ГЁ 50 Гў ГЄГ®Г­ГҐГ¶ ГЁ Гў Г­Г Г·Г Г«Г®
     array.insertAtEnd(35);
     array.insertAtBeginning(50);
-
-    // Выводим все элементы массива
     for (int j = 0; j < array.getLength(); j++)
         std::cout << array[j] << " ";
 
-	return 0;
+    return 0;
 }
